62f7bf8cffb6e5ea78684784b0afec9a00a5a4df
Yajat <yajatapps3@gmail.com>
2021-10-31 14:29:53 -0700

    Do not allow javascript for certain websites that don't allow to read news without login/subscription.

--- app/src/main/kotlin/com/yajatkumar/newsapp/ReaderActivity.kt
+++ app/src/main/kotlin/com/yajatkumar/newsapp/ReaderActivity.kt
@@ -13,28 +13,33 @@ class ReaderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val newsWebView = WebView(this)
-        newsWebView.webViewClient = WebViewClient()
-
-        // Enable javascript for the webView
-        val webSettings: WebSettings = newsWebView.settings
-        webSettings.javaScriptEnabled = true
-
-        setContentView(newsWebView)
-
         val title = intent.getStringExtra("title")
-        val url = intent.getStringExtra("url")
+        var url = intent.getStringExtra("url")
 
         if (title == null || url == null || title.isEmpty() || url.isEmpty()) {
             // Exit activity if title or url was not set
             finish()
         }
 
+        if (url != null)
+            url = url.replace("http://", "https://")
+
+        val newsWebView = WebView(this)
+        newsWebView.webViewClient = WebViewClient()
+
+        if (url != null && allowedJS(url)) {
+            // Enable javascript for the webView
+            val webSettings: WebSettings = newsWebView.settings
+            webSettings.javaScriptEnabled = true
+        }
+
+        setContentView(newsWebView)
+
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title = title
 
         if (url != null) {
-            newsWebView.loadUrl(url.replace("http://", "https://"))
+            newsWebView.loadUrl(url)
         }
     }
 
@@ -46,6 +51,22 @@ class ReaderActivity : AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    // Websites that will not be allowed to use javascript in web view
+    private val noJSlist =
+        listOf(
+            "https://www.nytimes.com",
+            "https://www.washingtonpost.com"
+        )
+
+    // Determine if websites can use javascript
+    private fun allowedJS(url: String): Boolean {
+        for (i in noJSlist) {
+            if (url.contains(i))
+                return false
+        }
+        return true
     }
 
 }
